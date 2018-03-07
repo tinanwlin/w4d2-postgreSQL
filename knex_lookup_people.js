948b63ffaa67e30bda75657827622da4f9095817
tinanwlin <tinanwlin@gmail.com>
2018-03-07 02:31:20 +0000

    Use knex lib to lookup famous_people table

---
+++ knex_lookup_people.js
@@ -0,0 +1,38 @@
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'development',
+    password: 'development',
+    database: 'vagrant',
+    port: 5432,
+    ssl: true
+  }
+});
+
+const nameARGV = process.argv[2];
+
+function outputDate(dateString) {
+  const newDate = new Date(dateString);
+  const year = newDate.getFullYear();
+  const month = (newDate.getMonth() < 9 ? '0' : '') + (newDate.getMonth() + 1);
+  const date = newDate.getDate();
+  return year + "-" + month + "-" + date;
+}
+
+knex.select('*').from('famous_people')
+  .where('first_name', nameARGV)
+  .orWhere('last_name', nameARGV)
+  .asCallback(function (err, rows) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`Found ${rows.length} person(s) by the name '${nameARGV}':`);
+      for (var i = 0; i < rows.length; i++) {
+        console.log(`- ${rows[i].id}: ${rows[i].first_name} ${rows[i].last_name}, born '${outputDate(rows[i].birthday)}'`)
+      }
+    }
+    knex.destroy();
+  });
+  
+
